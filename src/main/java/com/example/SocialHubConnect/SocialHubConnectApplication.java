c644d46ee3fb44e726d89302639a26e5922b6de7
Jonathan Tavarez <jtavarez@tjss.us>
2024-06-06 10:49:22 -0400

    first commit project dependency and database connections

---
+++ src/main/java/com/example/SocialHubConnect/SocialHubConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocialHubConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialHubConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialHubConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SocialHubConnect/SocialHubConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialHubConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialHubConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
